28e16af9ce7f1c0e6749e6500e3029eecb161b01
Jaydeep <jaysgaikwad123@gmail.com>
2026-01-04 23:29:47 +0530

    Created login page in XML

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,9 +6,7 @@ plugins {
 
 android {
     namespace = "com.jaydeep.activitylifecycle"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.jaydeep.activitylifecycle"
@@ -50,6 +48,10 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/jaydeep/activitylifecycle/MainActivity.kt
+++ app/src/main/java/com/jaydeep/activitylifecycle/MainActivity.kt
@@ -1,55 +1,16 @@
 package com.jaydeep.activitylifecycle
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.jaydeep.activitylifecycle.ui.theme.ActivityLifecycleTheme
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity() {
+   override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.scrollview_example)
-        println("Oncreate")
+        setContentView(R.layout.activity_main)
+        title = "Ironman"
     }
-
-    override fun onStart() {
-        super.onStart()
-        println("Onstart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        println("OnResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        println("OnPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        println("OnStop")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        println("OnRestart")
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        println("OnDestroy")
-    }
-
-
 }
+
 
